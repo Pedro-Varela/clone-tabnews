7e06d5f72963a4e693bec52fa439a791c65a6ae9
Pedro Varela <64024622+Pedro-Varela@users.noreply.github.com>
2023-09-15 19:43:35 +0000

    Adicionado a foto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Oi amor, te amo muito!</h1>
+    return <><h1>Oi amor, te amo muito!</h1><a href="https://freeimage.host/i/JHEpOvt"><img src="https://iili.io/JHEpOvt.md.jpg" alt="JHEpOvt.md.jpg" border="0" /></a></>
+    
 }
 
 export default Home;
